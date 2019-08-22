54c3dda002ed08dbb2595c1b9dec91531c088dd6
guoshengqiu <1172744949@qq.com>
2019-08-22 14:16:22 +0800

    问题“sign页路由不能跳转”

---
+++ vue-loading/vue-practice/src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>首页</span>
+    <router-link :to="{path: '/sign', query: {id: 123}}">登录</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('a')
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-loading/vue-practice/src/views/sign/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span>登录</span>
+    <router-link :to="{path: '/', query: {id: 456}}">返回</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    id () {
+      return this.$router.query.id
+    }
+  },
+  mounted () {
+    console.log(this.id)
+  }
+}
+</script>
+
+<style>
+
+</style>

f4aa6760fa9772d5c7f228cf36fd5fd08d6f586e
guoshengqiu <1172744949@qq.com>
2019-08-30 18:41:04 +0800

    添加返回顶部功能

---
+++ vue-loading/vue-practice/src/components/ToTop/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div @click="handle">
+    返回顶部
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToTop',
+  methods: {
+    handle () {
+      console.log('点击一次')
+      this.$emit('returnTop')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- vue-loading/vue-practice/src/views/home/index.vue
+++ vue-loading/vue-practice/src/views/home/index.vue
@@ -1,19 +1,66 @@
 <template>
   <div>
-    <span>首页</span>
-    <router-link :to="{path: '/sign', query: {id: 123}}">登录</router-link>
-    <!-- <router-view></router-view> -->
+    <div class="test">
+      <span>首页</span>
+      <router-link :to="{path: '/sign', query: {id: 123}}">登录</router-link>
+      <!-- <router-view></router-view> -->
+      <div class="content-top">模块1</div>
+      <div class="content-middle">模块2</div>
+      <div class="content-bottom">模块2</div>
+      <footer>底部</footer>
+      <!-- <div class="to-top" @click="toTop">回到顶部</div> -->
+      <ToTop class="to-top" @returnTop="toTop"/>
+    </div>
   </div>
 </template>
 
 <script>
+import ToTop from '@/components/ToTop'
 export default {
+  components: { ToTop },
   created () {
     console.log('a')
+  },
+  methods: {
+    toTop () {
+      document.body.scrollTop = 0
+      document.documentElement.scrollTop = 0
+      console.log('回到顶部一次')
+    }
   }
 }
 </script>
 
 <style>
-
+  .test {
+    position: relative;
+    top: 0 ;
+    left: 0;
+  }
+  .to-top {
+    position: fixed;
+    bottom: 200px;
+    right: 0px;
+    height: 90px;
+    width: 30px;
+    background-color: blue;
+  }
+  .content-top {
+    height: 500px;
+    width: 800px;
+    margin: 20px;
+    background-color: gray;
+  }
+  .content-middle {
+    height: 500px;
+    width: 800px;
+    margin: 20px;
+    background-color: red;
+  }
+  .content-bottom {
+    height: 500px;
+    width: 1000px;
+    margin: 20px;
+    background-color: yellow;
+  }
 </style>
